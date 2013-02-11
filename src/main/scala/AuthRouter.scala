ded9d9813c9e73767e31cfc09d644b9d11169770
Nikolay Zubarev <zng-14@yandex.ru>
2013-02-11 12:19:09 +0400

    Add block login and logout

--- src/main/scala/AuthRouter.scala
+++ src/main/scala/AuthRouter.scala
@@ -11,7 +11,27 @@ package net.whiteants
 import pro.savant.circumflex._, core._, web._
 
 class AuthRouter extends Router {
+  //блок входа
   get("/login") = ftl("/auth/login.ftl")
 
-
+  post("/login") = {
+    val email = param("email")
+    val password = param("passwordSha246")
+    val user = User.findByEmail(email, password)
+    if (User.isEmpty) {
+      //не удачно
+      flash.update("msg", msg.fmt(user.login.failed) )
+      sendRedirect("/auth/login")
+    } else {
+      //удачно
+      flash.update("msg", msq.fmt(user.login.managed))
+      request.session.update("principal")
+      sendRedirect("/")
+    }
+  }
+  //блок выхода
+  get("logout") = {
+    request.session.remove("principal")
+    sendRedirect("/")
+  }
 }
